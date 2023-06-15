e2dca00df02b823590844eb0d523b968bd5dc35a
Tiago Vargas <tiago.ce10@outlook.com>
2023-06-15 12:27:43 -0300

    test classifying path with repeating nodes as invalid

--- test_solver.py
+++ test_solver.py
@@ -4,36 +4,44 @@ from solver import AssemblyLine, System
 
 
 class TestPathValidator:
-	def test_system_single_line_with_consecutive_nodes(self):
-		a = AssemblyLine(name='A', nodes=3)
-		path = [a[0], a[1], a[2]]
-		system = System(assembly_lines=[a])
+	class TestSystemWithSingleAssemblyLine:
+		def test_system_single_line_with_consecutive_nodes(self):
+			a = AssemblyLine(name='A', nodes=3)
+			path = [a[0], a[1], a[2]]
+			system = System(assembly_lines=[a])
 
-		assert system.is_valid(path)
+			assert system.is_valid(path)
 
-	def test_system_single_line_with_nodes_in_reverse_order(self):
-		a = AssemblyLine(name='A', nodes=3)
-		path = [a[2], a[1], a[0]]
-		system = System(assembly_lines=[a])
+		def test_system_single_line_with_nodes_in_reverse_order(self):
+			a = AssemblyLine(name='A', nodes=3)
+			path = [a[2], a[1], a[0]]
+			system = System(assembly_lines=[a])
 
-		assert not system.is_valid(path)
+			assert not system.is_valid(path)
 
-	def test_system_single_line_with_nodes_in_wrong_order(self):
-		a = AssemblyLine(name='A', nodes=5)
-		path = [a[2], a[0], a[4], a[1], a[3]]
-		system = System(assembly_lines=[a])
+		def test_system_single_line_with_nodes_in_wrong_order(self):
+			a = AssemblyLine(name='A', nodes=5)
+			path = [a[2], a[0], a[4], a[1], a[3]]
+			system = System(assembly_lines=[a])
 
-		assert not system.is_valid(path)
+			assert not system.is_valid(path)
 
-	def test_raising_error_when_invalid_subscript(self):
-		a = AssemblyLine(name='A', nodes=3)
+		def test_raising_error_when_invalid_subscript(self):
+			a = AssemblyLine(name='A', nodes=3)
 
-		with pytest.raises(IndexError):
-			path = [a[0], a[1], a[2], a[3]]
+			with pytest.raises(IndexError):
+				path = [a[0], a[1], a[2], a[3]]
 
-	def test_classifying_path_with_too_few_nodes_as_invalid(self):
-		a = AssemblyLine(name='A', nodes=10)
-		path = [a[0], a[1], a[2]]
-		system = System(assembly_lines=[a])
+		def test_classifying_path_with_too_few_nodes_as_invalid(self):
+			a = AssemblyLine(name='A', nodes=10)
+			path = [a[0], a[1], a[2]]
+			system = System(assembly_lines=[a])
 
-		assert not system.is_valid(path)
+			assert not system.is_valid(path)
+
+		def test_repeating_nodes_as_invalid(self):
+			a = AssemblyLine(name='A', nodes=3)
+			path = [a[0], a[0], a[1]]
+			system = System(assembly_lines=[a])
+
+			assert not system.is_valid(path)
