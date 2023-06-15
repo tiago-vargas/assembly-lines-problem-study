022d81483c2f20a3d9f8bb80435a454c776dd794
Tiago Vargas <tiago.ce10@outlook.com>
2023-06-15 12:57:14 -0300

    test if nodes are conscutive with more assembly lines

--- test_solver.py
+++ test_solver.py
@@ -7,22 +7,25 @@ class TestPathValidator:
 	class TestSystemWithSingleAssemblyLine:
 		def test_system_single_line_with_consecutive_nodes(self):
 			a = AssemblyLine(name='A', nodes=3)
-			path = [a[0], a[1], a[2]]
 			system = System(assembly_lines=[a])
+
+			path = [a[0], a[1], a[2]]
 
 			assert system.is_valid(path)
 
 		def test_system_single_line_with_nodes_in_reverse_order(self):
 			a = AssemblyLine(name='A', nodes=3)
-			path = [a[2], a[1], a[0]]
 			system = System(assembly_lines=[a])
+
+			path = [a[2], a[1], a[0]]
 
 			assert not system.is_valid(path)
 
 		def test_system_single_line_with_nodes_in_wrong_order(self):
 			a = AssemblyLine(name='A', nodes=5)
-			path = [a[2], a[0], a[4], a[1], a[3]]
 			system = System(assembly_lines=[a])
+
+			path = [a[2], a[0], a[4], a[1], a[3]]
 
 			assert not system.is_valid(path)
 
@@ -34,14 +37,26 @@ class TestPathValidator:
 
 		def test_classifying_path_with_too_few_nodes_as_invalid(self):
 			a = AssemblyLine(name='A', nodes=10)
-			path = [a[0], a[1], a[2]]
 			system = System(assembly_lines=[a])
+
+			path = [a[0], a[1], a[2]]
 
 			assert not system.is_valid(path)
 
 		def test_repeating_nodes_as_invalid(self):
 			a = AssemblyLine(name='A', nodes=3)
-			path = [a[0], a[0], a[1]]
 			system = System(assembly_lines=[a])
 
+			path = [a[0], a[0], a[1]]
+
 			assert not system.is_valid(path)
+
+	class TestSystemWithTwoAssemblyLines:
+		def test_if_nodes_are_consecutive(self):
+			a = AssemblyLine(name='A', nodes=4)
+			b = AssemblyLine(name='B', nodes=4)
+			system = System(assembly_lines=[a, b])
+
+			path = [a[0], b[1], b[2], a[3]]
+
+			assert system.is_valid(path)
