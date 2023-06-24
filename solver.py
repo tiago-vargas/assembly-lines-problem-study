efec9e860c874f78de94889bbf6dc5143d97eee3
Tiago Vargas <tiago.ce10@outlook.com>
2023-06-24 00:54:03 -0300

    merge iterative algorithm

--- solver.py
+++ solver.py
@@ -1,3 +1,30 @@
+class IterativeSystem:
+	def __init__(self, entries: list[int], stations: list[list[int]], transitions: list[list[int]], exits: list[int]) -> None:
+		self.entries = entries
+		self.stations = stations
+		self.transitions = transitions
+		self.exits = exits
+
+		n = len(self.stations[0])
+		self.memoire = [[-1] * n, [-1] * n]
+
+	def get_optimal_time(self) -> int:
+		n = len(self.stations[0])
+		for index in range(0, n):
+			for line in [0, 1]:
+				self.memoire[line][index] = self.get_optimal_time_to_get_to_station(line, index)
+
+		return min(self.memoire[0][n - 1] + self.exits[0],
+				   self.memoire[1][n - 1] + self.exits[1])
+
+	def get_optimal_time_to_get_to_station(self, line: int, index: int) -> int:
+		if index == 0:
+			return self.entries[line] + self.stations[line][index]
+		else:
+			other_line = 1 - line
+			return min(self.memoire[line][index - 1], self.memoire[other_line][index - 1] + self.transitions[other_line][index - 1]) + self.stations[line][index]
+
+
 class RecursiveSystem:
 	def __init__(self, entries: list[int], stations: list[list[int]], transitions: list[list[int]], exits: list[int]) -> None:
 		self.entries = entries
@@ -7,7 +34,7 @@ class RecursiveSystem:
 
 		any_of_the_stations = self.stations[0]
 		n = len(any_of_the_stations)
-		self.memoire = [[0] * n, [0] * n]
+		self.memoire = [[-1] * n, [-1] * n]
 
 	def get_optimal_time(self) -> int:
 		n = len(self.stations[0])
@@ -16,7 +43,7 @@ class RecursiveSystem:
 		           self.optimal_time_to_get_to_station(1, last_station_index) + self.exits[1])
 
 	def optimal_time_to_get_to_station(self, line: int, index: int) -> int:
-		if self.memoire[line][index] != 0:
+		if self.memoire[line][index] != -1:
 			return self.memoire[line][index]
 
 		is_first_station = (index == 0)
--- test_solver.py
+++ test_solver.py
@@ -1,50 +1,93 @@
-from solver import RecursiveSystem
+from solver import IterativeSystem, RecursiveSystem
 
 
-def test_best_path_being_line_1():
-	entries = [1, 1000]
-	stations = [[1, 2, 3], [1000, 2000, 3000]]
-	transitions = [[1000, 1000], [1000, 2000]]
-	exits = [1, 1000]
-	system = RecursiveSystem(entries, stations, transitions, exits)
+class TestIterativeSystem:
+	def test_best_path_being_line_1(self):
+		entries = [1, 1000]
+		stations = [[1, 2, 3], [1000, 2000, 3000]]
+		transitions = [[1000, 1000], [1000, 2000]]
+		exits = [1, 1000]
+		system = IterativeSystem(entries, stations, transitions, exits)
 
-	optimal_time = system.get_optimal_time()
+		optimal_time = system.get_optimal_time()
 
-	assert optimal_time == 1 + 1 + 2 + 3 + 1
+		assert optimal_time == 1 + 1 + 2 + 3 + 1
+
+	def test_best_path_being_line_2(self):
+		entries = [1000, 1]
+		stations = [[1000, 2000, 3000], [1, 2, 3]]
+		transitions = [[1000, 1000], [1000, 2000]]
+		exits = [1000, 1]
+		system = IterativeSystem(entries, stations, transitions, exits)
+
+		optimal_time = system.get_optimal_time()
+
+		assert optimal_time == 1 + 1 + 2 + 3 + 1
+
+	def test_with_transitions(self):
+		entries = [10, 1000]
+		stations = [[1, 2000, 3], [1000, 2, 3000]]
+		transitions = [[10, 20], [10, 20]]
+		exits = [10, 1000]
+		system = IterativeSystem(entries, stations, transitions, exits)
+
+		optimal_time = system.get_optimal_time()
+
+		assert optimal_time == 10 + 1 + 10 + 2 + 20 + 3 + 10
+
+	def test_a_system_more_difficult_to_get_optimal_time(self):
+		entries = [1, 1]
+		stations = [[1, 2000, 3], [1000, 2, 3000]]
+		transitions = [[10, 20], [10, 20]]
+		exits = [10, 1000]
+		system = IterativeSystem(entries, stations, transitions, exits)
+
+		optimal_time = system.get_optimal_time()
+
+		assert optimal_time == 1 + 1 + 10 + 2 + 20 + 3 + 10
 
 
-def test_best_path_being_line_2():
-	entries = [1000, 1]
-	stations = [[1000, 2000, 3000], [1, 2, 3]]
-	transitions = [[1000, 1000], [1000, 2000]]
-	exits = [1000, 1]
-	system = RecursiveSystem(entries, stations, transitions, exits)
+class TestRecursiveSystem:
+	def test_best_path_being_line_1(self):
+		entries = [1, 1000]
+		stations = [[1, 2, 3], [1000, 2000, 3000]]
+		transitions = [[1000, 1000], [1000, 2000]]
+		exits = [1, 1000]
+		system = RecursiveSystem(entries, stations, transitions, exits)
 
-	optimal_time = system.get_optimal_time()
+		optimal_time = system.get_optimal_time()
 
-	assert optimal_time == 1 + 1 + 2 + 3 + 1
+		assert optimal_time == 1 + 1 + 2 + 3 + 1
 
+	def test_best_path_being_line_2(self):
+		entries = [1000, 1]
+		stations = [[1000, 2000, 3000], [1, 2, 3]]
+		transitions = [[1000, 1000], [1000, 2000]]
+		exits = [1000, 1]
+		system = RecursiveSystem(entries, stations, transitions, exits)
 
-def test_with_transitions():
-	entries = [10, 1000]
-	stations = [[1, 2000, 3], [1000, 2, 3000]]
-	transitions = [[10, 20], [10, 20]]
-	exits = [10, 1000]
-	system = RecursiveSystem(entries, stations, transitions, exits)
+		optimal_time = system.get_optimal_time()
 
-	optimal_time = system.get_optimal_time()
+		assert optimal_time == 1 + 1 + 2 + 3 + 1
 
-	assert optimal_time == 10 + 1 + 10 + 2 + 20 + 3 + 10
+	def test_with_transitions(self):
+		entries = [10, 1000]
+		stations = [[1, 2000, 3], [1000, 2, 3000]]
+		transitions = [[10, 20], [10, 20]]
+		exits = [10, 1000]
+		system = RecursiveSystem(entries, stations, transitions, exits)
 
+		optimal_time = system.get_optimal_time()
 
+		assert optimal_time == 10 + 1 + 10 + 2 + 20 + 3 + 10
 
-def test_a_system_more_difficult_to_get_optimal_time():
-	entries = [1, 1]
-	stations = [[1, 2000, 3], [1000, 2, 3000]]
-	transitions = [[10, 20], [10, 20]]
-	exits = [10, 1000]
-	system = RecursiveSystem(entries, stations, transitions, exits)
+	def test_a_system_more_difficult_to_get_optimal_time(self):
+		entries = [1, 1]
+		stations = [[1, 2000, 3], [1000, 2, 3000]]
+		transitions = [[10, 20], [10, 20]]
+		exits = [10, 1000]
+		system = RecursiveSystem(entries, stations, transitions, exits)
 
-	optimal_time = system.get_optimal_time()
+		optimal_time = system.get_optimal_time()
 
-	assert optimal_time == 1 + 1 + 10 + 2 + 20 + 3 + 10
+		assert optimal_time == 1 + 1 + 10 + 2 + 20 + 3 + 10
